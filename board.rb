ccc3a58da5927eef155f68f374d2a4509c22971c
Kurosh Hesamshariati <kuroshhesamshariati@gmail.com>
2023-01-07 15:12:36 +0330

    add class variable @@win_scenarios to Board class

--- board.rb
+++ board.rb
@@ -2,6 +2,8 @@
 
 # Class that holds boards info and methods
 class Board
+  @@win_scenarios = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
   attr_reader :player1, :player2
 
   def initialize(player1, player2)
