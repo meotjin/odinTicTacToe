7aad9c2de790f27608f40c7a0d1a547e05f511d1
Kurosh Hesamshariati <kuroshhesamshariati@gmail.com>
2023-01-07 14:57:29 +0330

    add picks instance array and add_pick() to Player

--- player.rb
+++ player.rb
@@ -2,10 +2,15 @@
 
 # this class holds player info
 class Player
-  attr_reader :name, :symbol
+  attr_reader :name, :symbol, :picks
 
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
+    @picks = []
+  end
+
+  def add_pick(pick)
+    picks.push(pick)
   end
 end
